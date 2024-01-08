d8d9072756cef0839aba3f318935ecf43799aef8
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2024-01-08 16:30:26 +0500

    Add todo-entity to todo-main

---
+++ todo-main/src/test/java/io/seoleir/todomain/TodoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seoleir.todomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
